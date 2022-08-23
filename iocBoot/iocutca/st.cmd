7c78b5972c6c7817e8255e88f13bb06774b8727e
Érico Nogueira <erico.rolim@lnls.br>
2022-08-23 16:11:50 -0300

    Remove unused DBs and add Asyn.

--- iocBoot/iocutca/st.cmd
+++ iocBoot/iocutca/st.cmd
@@ -11,18 +11,8 @@ cd "${TOP}"
 dbLoadDatabase "dbd/utca.dbd"
 utca_registerRecordDeviceDriver pdbbase
 
-## Load record instances
-dbLoadTemplate "db/user.substitutions"
-dbLoadRecords "db/dbSubExample.db", "user=ericorolim"
-
-## Set this to see messages from mySub
-#var mySubDebug 1
-
 ## Run this to trace the stages of iocInit
 #traceIocInit
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
-
-## Start any sequence programs
-#seq sncExample, "user=ericorolim"
