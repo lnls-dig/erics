b2b5c82f15d97a714accb9decfed881c5ed19fba
Érico Nogueira <erico.rolim@lnls.br>
2022-08-25 08:30:33 -0300

    rtmlamp: move to new base style.
    - the base port name comes from the class itself, so we don't need to
      pass it to the underlying functions
    - we can have more than one port of each kind, so add them in a clean
      way
    - fix the name of the registrar function as well

--- utcaApp/src/rtmlamp.cpp
+++ utcaApp/src/rtmlamp.cpp
@@ -1,3 +1,5 @@
+#include <string>
+
 #include <iocsh.h>
 #include <epicsExport.h>
 
@@ -6,30 +8,33 @@
 #include "lamp.h"
 
 class RtmLamp: public asynPortDriver {
-
   public:
-    RtmLamp(const char *portName):
-      asynPortDriver(portName, 10, -1, -1, 0, 1, 0, 0)
+    RtmLamp(int port_number):
+      asynPortDriver(
+          (std::string("RTMLAMP") + std::to_string(port_number)).c_str(),
+          12, /* channels */
+          -1, -1, /* enable all interfaces and interrupts */
+          0, 1, 0, 0 /* no flags, auto connect, default priority and stack size */
+      )
     {
-        printf("hallo %s\n", portName);
     }
 };
 
 /* Configuration routine.  Called directly, or from the iocsh function below */
 extern "C" {
     /* EPICS iocsh shell commands */
-    static const iocshArg initArg0 = { "portName", iocshArgString};
-    static const iocshArg * const initArgs[] = {&initArg0};
-    static const iocshFuncDef initFuncDef = {"RtmLamp", 1,initArgs};
+    static const iocshArg initArg0 = {"portNumber", iocshArgInt};
+    static const iocshArg *initArgs[] = {&initArg0};
+    static const iocshFuncDef initFuncDef = {"RtmLamp", 1, initArgs};
     static void initCallFunc(const iocshArgBuf *args)
     {
-        new RtmLamp(args[0].sval);
+        new RtmLamp(args[0].ival);
     }
 
-    void testSupportRegister(void)
+    static void registerRtmLamp(void)
     {
-        iocshRegister(&initFuncDef,initCallFunc);
+        iocshRegister(&initFuncDef, initCallFunc);
     }
 
-    epicsExportRegistrar(testSupportRegister);
+    epicsExportRegistrar(registerRtmLamp);
 }
