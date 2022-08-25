293a03dc59b25817e227fb93869ab9a2b53cae69
Érico Nogueira <erico.rolim@lnls.br>
2022-08-25 11:39:39 -0300

    pcie-single: add pcie_bars singleton.
    This will be the object used by all the other classes to access the
    hardware.

---
+++ utcaApp/src/pcie-single.cpp
@@ -0,0 +1,33 @@
+#include <stdexcept>
+
+#include <iocsh.h>
+#include <epicsExport.h>
+
+#include "pcie-open.h"
+
+#include "pcie-single.h"
+
+struct pcie_bars bars;
+
+extern "C" {
+    static const iocshArg initArg0 = {"slotName", iocshArgString};
+    static const iocshArg *initArgs[] = {&initArg0};
+    static const iocshFuncDef initFuncDef = {"pcie", 1, initArgs};
+    static void initCallFunc(const iocshArgBuf *args)
+    {
+        try {
+            std::string slot_name = args[0].sval;
+            dev_open_slot(bars, slot_name.c_str());
+        } catch (std::runtime_error &e) {
+            /* FIXME: do something with this exception,
+             * so far we are only avoiding propagating it through C */
+        }
+    }
+
+    static void registerPcie(void)
+    {
+        iocshRegister(&initFuncDef, initCallFunc);
+    }
+
+    epicsExportRegistrar(registerPcie);
+}
---
+++ utcaApp/src/pcie-single.h
@@ -0,0 +1,9 @@
+#ifndef PCIE_SINGLE_H
+#define PCIE_SINGLE_H
+
+#include "pcie.h"
+
+/* pcie_bars singleton */
+extern struct pcie_bars bars;
+
+#endif
