65fd2870b3a9db8c6d97c51bbb8c00e20ca3f36f
Érico Nogueira <erico.rolim@lnls.br>
2022-08-26 16:23:12 -0300

    rtmlamp: implement writeInt32.
    - remove setIntegerParam calls from constructor; these are now expected
      to be handled elsewhere
    - we use the ctl interface in the "single channel write" mode

--- utcaApp/src/rtmlamp.cpp
+++ utcaApp/src/rtmlamp.cpp
@@ -22,6 +22,9 @@ class RtmLamp: public asynPortDriver {
         p_pi_sp, p_dac, p_eff_adc, p_eff_dac, p_eff_sp,
         p_trigen;
 
+    /* XXX: update these when new p_* variables are added */
+    const int &first_parameter = p_psstatus, &last_parameter = p_trigen;
+
   public:
     RtmLamp(int port_number):
       asynPortDriver(
@@ -62,17 +65,70 @@ class RtmLamp: public asynPortDriver {
             {"TRIG_EN", p_trigen},
         };
 
+        /* here, we don't call setIntegerParam to initialize these parameters:
+         * - readback ones are expected to be initialized soon via SCAN
+         * - setpoint ones are expected to be initialized whenever their records
+         *   are written into: this also allows us to avoid writing into fields
+         *   which aren't always supported, as long as the PVs haven't been written
+         *   into */
+
         for (auto &v: name_and_index) {
             createParam(std::get<0>(v), asynParamInt32, &std::get<1>(v));
-            setIntegerParam(0, std::get<1>(v), 0);
         }
 
         for (auto &v: name_and_index_channel) {
             createParam(std::get<0>(v), asynParamInt32, &std::get<1>(v));
-            for (unsigned addr = 0; addr < number_of_channels; addr++) {
-                setIntegerParam(addr, std::get<1>(v), 0);
-            }
         }
+    }
+
+    asynStatus writeInt32(asynUser *pasynUser, epicsInt32 value)
+    {
+        const int function = pasynUser->reason;
+        int addr;
+        getAddress(pasynUser, &addr);
+
+        if (function < first_parameter || function > last_parameter) {
+            return asynPortDriver::writeInt32(pasynUser, value);
+        }
+
+        setIntegerParam(addr, function, value);
+        callParamCallbacks(addr);
+
+        ctl.channel = addr;
+
+        auto write_param = [addr, this](auto fn, auto &ctl_value) {
+            epicsInt32 tmp;
+            /* we expect success, unless the parameter hasn't been initialized yet */
+            if (getIntegerParam(addr, fn, &tmp) == asynSuccess)
+                ctl_value = tmp;
+        };
+
+        write_param(p_pwrstate, ctl.amp_enable);
+        write_param(p_opmode, ctl.mode_numeric);
+        write_param(p_loopkp, ctl.pi_kp);
+        write_param(p_loopti, ctl.pi_ti);
+        write_param(p_testwaveperiod, ctl.cnt);
+        write_param(p_testlim_a, ctl.limit_a);
+        write_param(p_testlim_b, ctl.limit_b);
+        write_param(p_pi_sp, ctl.pi_sp);
+        write_param(p_dac, ctl.dac);
+        /* FIXME: if someone writes into trigen, we'll error out continuously until
+         * being restarted */
+        write_param(p_trigen, ctl.trigger_enable);
+
+        try {
+            ctl.write_params();
+        } catch (std::runtime_error &e) {
+            const char *param_name;
+
+            getParamName(function, &param_name);
+            epicsSnprintf(pasynUser->errorMessage, pasynUser->errorMessageSize,
+                "writeInt32:%s: %s", param_name, e.what());
+
+            return asynError;
+        }
+
+        return asynSuccess;
     }
 };
 
